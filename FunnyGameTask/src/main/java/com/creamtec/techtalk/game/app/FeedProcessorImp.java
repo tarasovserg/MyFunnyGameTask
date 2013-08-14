a2b16330ed2c8bb0734a87757852ae2c31b64f37
tarasovserg <tarasovsergserg@gmail.com>
2013-08-14 18:28:58 +0300

    Some file update

--- FunnyGameTask/src/main/java/com/creamtec/techtalk/game/app/FeedProcessorImp.java
+++ FunnyGameTask/src/main/java/com/creamtec/techtalk/game/app/FeedProcessorImp.java
@@ -1,11 +1,17 @@
 package com.creamtec.techtalk.game.app;
 
+import java.awt.Image;
+import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+
+import javax.imageio.ImageIO;
 
 import com.creamtec.techtalk.game.api.Cell;
 import com.creamtec.techtalk.game.api.Feed;
 import com.creamtec.techtalk.game.api.FeedProcessor;
+import com.creamtec.techtalk.game.api.Icon;
 
 public class FeedProcessorImp  implements FeedProcessor{
 
@@ -14,15 +20,50 @@ public class FeedProcessorImp  implements FeedProcessor{
 		return arr;
 		
 	}
+	Random random = new Random();
 	
-	private List<Feed> feed;
+	private Image img;
+	
+	private static final int FEED_GENERATION_RATE = 10;
+	
+	/**
+	 * Check if cell is accessible for gamer
+	 * @param cell {@link Cell} object
+	 * @return
+	 */
+	private boolean checkCellForDead(Cell cell){
+		if(cell.getBorder().hasBottom()  && 
+				cell.getBorder().hasLeft()  &&
+				cell.getBorder().hasRight()  &&
+				cell.getBorder().hasTop()) {
+			return true;
+			
+		}
+		else {
+			return false;
+		}
+	}
+	
+	private List<Feed> feed = new ArrayList<Feed>();
 	
 	@Override
 	public boolean initialize(List<Cell> mazeDescription) {
-		// TODO Auto-generated method stub
+		img = ImageIO.read("");  
+		//initialize feed in every
+		for(Cell cell : mazeDescription) {
+			if(random.nextInt(FEED_GENERATION_RATE)== 0 &&
+					!checkCellForDead(cell)) {
+				
+				FeedImp feed = new FeedImp();
+				
+				IconImp icon =  new IconImp();
+				feed.setIcon();
+				feed.add(e);
+				
+			}
+		}
 		
 		
-		feed = new ArrayList<Feed>();
 		
 		
 		return true;
@@ -36,8 +77,7 @@ public class FeedProcessorImp  implements FeedProcessor{
 
 	@Override
 	public List<Feed> getFeed() {
-		// TODO Auto-generated method stub
-		return null;
+		return feed;
 	}
 
 }
